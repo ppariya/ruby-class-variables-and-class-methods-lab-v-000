2a95f94113969bc847d0c9ae3fb4a4f116b4a2ca
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-30 09:40:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,16 +26,15 @@ class Song
   end
 
   def self.genre_count
-    @genre_count = {}
-    counts = 0
-    @genre_count.each do |genre, count|
-      genre.each do |index|
-        if genre == index
-          count += 1
-        else
-          genre_count = {genre => counts}
-        end
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
       end
     end
-        
+    genre_count
+  end
+
 end
